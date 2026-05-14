ada23b1f4f0af85b5cf52db769a3c7201a5d9abf
Darshan S <darshans2727@gmail.com>
2026-05-14 21:44:39 +0530

    feat: add verifySupplier middleware

---
+++ src/middleware/suppliers.ts
@@ -0,0 +1,17 @@
+import type { NextFunction, Request, Response } from 'express'
+import { SuppliersRepository } from '../db/repository.js'
+import { ApiError } from '../errors.js'
+
+export async function verifySupplier(req: Request, res: Response, next: NextFunction) {
+  const userId = req.userId!
+
+  const supplier = await SuppliersRepository.getByUserId(userId)
+
+  if (!supplier) {
+    throw new ApiError('SUPPLIER_NOT_FOUND', 403)
+  }
+
+  req.supplierId = supplier.id
+
+  next()
+}
